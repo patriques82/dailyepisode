abed6d586ec0168f17eccdc8b45eb397f918b42b
Patrik Nygren <patriknygren82@gmail.com>
2018-08-05 17:58:09 +0200

    gradle modules more independent and sharing less in order to enable kotlin2js frontend module

--- web/src/main/kotlin/org/dailyepisode/Application.kt
+++ web/src/main/kotlin/org/dailyepisode/Application.kt
@@ -2,7 +2,6 @@ package org.dailyepisode
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
 class Application
