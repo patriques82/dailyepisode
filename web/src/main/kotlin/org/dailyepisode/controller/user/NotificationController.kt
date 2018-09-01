9f0a09872c1f23fcae0bf8ec9ce6a6b1c0946562
Patrik Nygren <patriknygren82@gmail.com>
2018-09-01 20:28:42 +0200

    email send test

---
+++ web/src/main/kotlin/org/dailyepisode/controller/user/NotificationController.kt
@@ -0,0 +1,24 @@
+package org.dailyepisode.controller.user
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.mail.SimpleMailMessage
+
+@RestController
+@RequestMapping("/api/mail")
+class NotificationController(val emailSender: JavaMailSender) {
+
+  @PostMapping("/test")
+  fun testEmail(): ResponseEntity<String> {
+    val message = SimpleMailMessage()
+    message.setTo("patrik.nygren@evolvetechnology.se")
+    message.setSubject("test")
+    message.setText("test")
+    emailSender.send(message)
+    return ResponseEntity("message sent", HttpStatus.OK)
+  }
+}
