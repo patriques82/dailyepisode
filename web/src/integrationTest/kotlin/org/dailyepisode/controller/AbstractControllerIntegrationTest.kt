be070c5b201e5a3a585ae5cae88bcc1dec8ff972
Patrik Nygren <patriknygren82@gmail.com>
2018-09-11 09:36:24 +0200

    refactor: common setup logic inherited

--- web/src/integrationTest/kotlin/org/dailyepisode/controller/AbstractControllerIntegrationTest.kt
+++ web/src/integrationTest/kotlin/org/dailyepisode/controller/AbstractControllerIntegrationTest.kt
@@ -1,11 +1,34 @@
 package org.dailyepisode.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.Before
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-abstract class AbstractControllerIntegratiionTest {
+abstract class AbstractControllerIntegrationTest {
+
+  @Autowired
+  protected lateinit var objectMapper: ObjectMapper
+  @Autowired
+  private lateinit var context: WebApplicationContext
+
+  protected lateinit var mockMvc: MockMvc
+
+  @Before
+  fun setUp() {
+    mockMvc = MockMvcBuilders
+      .webAppContextSetup(context)
+      .apply<DefaultMockMvcBuilder>(SecurityMockMvcConfigurers.springSecurity())
+      .build()
+  }
 
 }
--- web/src/integrationTest/kotlin/org/dailyepisode/controller/admin/AdminAccountControllerTest.kt
+++ web/src/integrationTest/kotlin/org/dailyepisode/controller/admin/AdminAccountControllerTest.kt
@@ -1,46 +1,25 @@
 package org.dailyepisode.controller.admin
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.dailyepisode.account.AccountEntity
 import org.dailyepisode.account.AccountRepository
-import org.dailyepisode.controller.AbstractControllerIntegratiionTest
+import org.dailyepisode.controller.AbstractControllerIntegrationTest
 import org.dailyepisode.dto.AccountRegistrationRequestDto
-import org.junit.Before
 import org.junit.Test
 import org.mockito.BDDMockito.given
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
-import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
 import java.util.*
 
-class AdminAccountControllerTest: AbstractControllerIntegratiionTest() {
+class AdminAccountControllerTest: AbstractControllerIntegrationTest() {
 
   @MockBean
   lateinit var accountRepository: AccountRepository
-  @Autowired
-  lateinit var objectMapper: ObjectMapper
-  @Autowired
-  lateinit var context: WebApplicationContext
-  lateinit var mockMvc: MockMvc
-
-  @Before
-  fun setUp() {
-    mockMvc = MockMvcBuilders
-      .webAppContextSetup(context)
-      .apply<DefaultMockMvcBuilder>(springSecurity())
-      .build()
-  }
 
   @Test
   @WithMockUser(roles = arrayOf("USER"))
